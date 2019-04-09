a9cd674513e1d92cd42405d93e81397f3aae40b8
Kyle McEntush <slatebitstudios@gmail.com>
2019-04-09 15:08:04 -0700

    Updated some flask templates

--- flask/live.py
+++ flask/live.py
@@ -1,5 +1,7 @@
 from flask import *
 import folium
+import math
+import re
 
 app = Flask(__name__)
 
@@ -7,20 +9,30 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 @app.route('/map', methods=['GET', 'POST'])
 def map():
-    # Get the user settings
+    
     if request.method == 'POST':
-    	numIncidents = request.form['numIncidents']
+        # Get form info
+        numIncidents = request.form['numIncidents']
+        center = request.form['center']
+        zoom = int(request.form['zoom'])
+
+        # Extract the map center
+        center = re.findall('LatLng\((.+), (.+)\)', center)[0]
+        center = [float(val) for val in center]
     else:
-    	numIncidents = 50
+        numIncidents = 50
+        zoom = 4
+        center = [35, -97]
 
     # Create the map
-    map_obj = folium.Map(location=[35, -97], zoom_start=4, tiles='Mapbox Bright', width='50%', height='50%')
+    mapObj = folium.Map(location=center, zoom_start=zoom, tiles='Mapbox Bright', width='75%', height='75%')
 
     # Extract the HTML and make it safe
-    map_html = Markup(map_obj.get_root().render())
-    print(map_html)
+    mapHTML = mapObj.get_root().render()
+    mapVar = re.findall('(map_[a-z0-9]+)', mapHTML)[0]
+    mapHTML = Markup(mapHTML)
 
-    return render_template('map.html', map=map_html, numIncidents=numIncidents)
+    return render_template('map.html', map=mapHTML, mapVar = mapVar, numIncidents=numIncidents)
 
 if __name__ == '__main__':
     app.run(debug=True)
