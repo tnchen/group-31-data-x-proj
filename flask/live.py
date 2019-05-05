d374d3e37de432d8b0aab2e26cb9d1247f2c9d2d
slatebitstudios@gmail.com <slatebitstudios@gmail.com>
2019-05-05 15:20:52 -0700

    Almost done adding ML to site

--- flask/live.py
+++ flask/live.py
@@ -74,7 +74,7 @@ def map():
         cluster.add_to(mapObj)
 
         # Add the state data
-        path = os.path.join(directory, 'us_states_20m.json')
+        path = os.path.join(directory, 'data/us_states_20m.json')
         folium.Choropleth(
             geo_data=path,
             name=selectedFactor,
@@ -96,6 +96,153 @@ def map():
     mapHTML = Markup(mapHTML)
 
     return render_template('map.html', map=mapHTML, mapVar=mapVar, numIncidents=numIncidents, factors=factors, numFactors=len(data.stateCols[1:]), selectedIndex=selectedIndex)
+
+
+@app.route('/ml', methods=['GET', 'POST'])
+def get():
+    # Define list of values
+    year_list = list(range(2005, 2026))
+    month_list = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    state_list = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado',
+                  'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois',
+                  'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland',
+                  'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana',
+                  'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York',
+                  'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania',
+                  'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah',
+                  'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
+    grade_list = ['A', 'A-', 'B', 'B+', 'B-', 'C', 'C+', 'C-', 'D', 'D+', 'D-', 'F']
+
+    if request.method == 'POST':
+        # Get form info
+        year = request.form['yearVal']
+        month = request.form['monthVal']
+        state = request.form['stateVal']
+        mentHealthRecSubmit2008 = request.form['mentHealthRecSubmit2008']
+        mentHealthRecSubmit2017 = request.form['mentHealthRecSubmit2017']
+        gunDenials2008 = request.form['gunDenials2008']
+        gunDenials2017 = request.form['gunDenials2017']
+        gunDeathRate2018 = request.form['gunDeathRate2018']
+        gunDeathRank = request.form['gunDeathRankVal']
+        gunOwnerRank = request.form['gunOwnerRankVal']
+        gunPerCap = request.form['gunPerCap']
+        numGunRegis = request.form['numGunRegis']
+        permitCost5Yr = request.form['permitCost5Yr']
+        happinessScore = request.form['happinessScore']
+        scoreGiffords = request.form['scoreGiffords']
+
+        # Extract the year
+        if year != '':
+            year = int(year)
+            if year < len(year_list):
+                year = year_list[year]
+        else:
+            year = 2019
+
+        # Extract the month
+        if month != '':
+            month = int(month)
+        else:
+            month = 0
+
+        # Extract the state
+        if state != '':
+            state = int(state)
+        else:
+            state = 0
+
+        # Extract the number of mental health records submitted in 2008
+        if mentHealthRecSubmit2008 != '':
+            mentHealthRecSubmit2008 = int(mentHealthRecSubmit2008)
+        else:
+            mentHealthRecSubmit2008 = 155
+
+        # Extract the number of mental health records submitted in 2017
+        if mentHealthRecSubmit2017 != '':
+            mentHealthRecSubmit2017 = int(mentHealthRecSubmit2017)
+        else:
+            mentHealthRecSubmit2017 = 155
+
+        # Extract the number of gun sale denials in 2008
+        if gunDenials2008 != '':
+            gunDenials2008 = int(gunDenials2008)
+        else:
+            gunDenials2008 = 32
+
+        # Extract the number of gun sale denials in 2017
+        if gunDenials2017 != '':
+            gunDenials2017 = int(gunDenials2017)
+        else:
+            gunDenials2017 = 125
+
+        # Extract the gun death rate in 2018
+        if gunDeathRate2018 != '':
+            gunDeathRate2018 = float(gunDeathRate2018) / 10
+        else:
+            gunDeathRate2018 = 22.9
+
+        # Extract the gun death rank
+        if gunDeathRank != '':
+            gunDeathRank = int(gunDeathRank)
+        else:
+            gunDeathRank = 1
+
+        # Extract the gun owner rank
+        if gunOwnerRank != '':
+            gunOwnerRank = int(gunOwnerRank)
+        else:
+            gunOwnerRank = 5
+
+        # Extract the guns per capita
+        if gunPerCap != '':
+            gunPerCap = float(gunPerCap) / 100
+        else:
+            gunPerCap = 33.15
+
+        # Extract the number of guns registered
+        if numGunRegis != '':
+            numGunRegis = int(numGunRegis)
+        else:
+            numGunRegis = 161641
+
+        # Extract the 5 year permit cost
+        if permitCost5Yr != '':
+            permitCost5Yr = int(permitCost5Yr)
+        else:
+            permitCost5Yr = 25
+
+        # Extract the happiness score
+        if happinessScore != '':
+            happinessScore = int(happinessScore)
+        else:
+            happinessScore = 4
+
+        # Extract the Gifford's gun safety score
+        if scoreGiffords != '':
+            scoreGiffords = int(scoreGiffords)
+        else:
+            scoreGiffords = 11
+    else:
+        year = 2019
+        month = 0  # based on month_list (NOTE: 0 is January here, but in ML 1 is January)
+        mentHealthRecSubmit2008 = 155
+        mentHealthRecSubmit2017 = 5036
+        gunDenials2008 = 32
+        gunDenials2017 = 125
+        gunDeathRate2018 = 22.9  # per 100k people
+        gunDeathRank = 1  # zero indexed
+        gunOwnerRank = 5  # zero indexed
+        gunPerCap = 33.15
+        numGunRegis = 161641
+        permitCost5Yr = 25
+        happinessScore = 4
+        state = 0  # based on state_list
+        scoreGiffords = 11  # based on grade_list
+
+    return render_template('ml.html', monthList=month_list, stateList = state_list, gradeList=grade_list, selectedYear=year, selectedMonth=month, selectedState=state, mentHealthRecSubmit2008=mentHealthRecSubmit2008,
+                           mentHealthRecSubmit2017=mentHealthRecSubmit2017, gunDenials2008=gunDenials2008, gunDenials2017=gunDenials2017, gunDeathRate2018=gunDeathRate2018 * 10,
+                           gunDeathRank=gunDeathRank, gunOwnerRank=gunOwnerRank, gunPerCap=gunPerCap * 100, numGunRegis=numGunRegis, permitCost5Yr=permitCost5Yr,
+                           happinessScore=happinessScore, scoreGiffords=scoreGiffords)
 
 if __name__ == '__main__':
     app.run(debug=True)
